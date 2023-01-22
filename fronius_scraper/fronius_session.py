ef1e3c40ba2a44f435a6f7c2a4f88cb8d2410297
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2023-01-22 11:34:20 +0000

    fronius_session.py: Format with black

--- fronius_scraper/fronius_session.py
+++ fronius_scraper/fronius_session.py
@@ -3,37 +3,33 @@ from bs4 import BeautifulSoup as bs
 import urllib.parse as ulparse
 import requests as rq
 
+
 class FroniusSession:
     """Class resonsible for managing the entire correspondence with fronius."""
+
     landing_url = "https://www.solarweb.com/"
     login_url = "https://www.solarweb.com/Account/ExternalLogin"
     login_form_post_url = "https://login.fronius.com/commonauth"
     chart_url = "https://www.solarweb.com/Chart/GetChartNew"
-    key_pattern = re.compile("(?<=&sessionDataKey=)[a-z0-9\-]*")   
+    key_pattern = re.compile("(?<=&sessionDataKey=)[a-z0-9\-]*")
 
     def __init__(self, user, password, id):
         self.session = rq.Session()
         self.key_pattern = re.compile("(?<=&sessionDataKey=)[a-z0-9\-]*")
         self.session_key = None
         self.is_logged_in = False
-        self.secret = {
-            "username": user,
-            "password": password,
-            "id": id
-        }
+        self.secret = {"username": user, "password": password, "id": id}
 
         self.login()
 
     def login(self):
         """Get all necessary data and cookies to perform all operations."""
-        
+
         # Just to get the __RequestVerificationToken
         # TODO Add handling of case that solarweb is not reachable.
-        _ = self.session.get(url = self.landing_url)
+        _ = self.session.get(url=self.landing_url)
 
-        login_page_resp = self.session.get(
-            self.login_url,
-            allow_redirects = True)
+        login_page_resp = self.session.get(self.login_url, allow_redirects=True)
 
         for line in login_page_resp.iter_lines():
             match = re.search(self.key_pattern, line.decode())
@@ -41,14 +37,17 @@ class FroniusSession:
                 self.session_key = match.group()
 
         login_form_resp = self.session.post(
-            url = self.login_form_post_url,
-            data = {
-                    "username": self.secret["username"],
-                    "password": self.secret["password"],
-                    "sessionDataKey": self.session_key})
+            url=self.login_form_post_url,
+            data={
+                "username": self.secret["username"],
+                "password": self.secret["password"],
+                "sessionDataKey": self.session_key,
+            },
+        )
 
-        callback_url = ulparse.parse_qs(
-            ulparse.urlparse(login_page_resp.url).query)["redirect_uri"][0]
+        callback_url = ulparse.parse_qs(ulparse.urlparse(login_page_resp.url).query)[
+            "redirect_uri"
+        ][0]
 
         login_soup = bs(login_form_resp.content, "lxml")
 
@@ -57,16 +56,14 @@ class FroniusSession:
             "id_token": None,
             "state": None,
             "AuthenticatedIdPs": None,
-            "session_state": None}
+            "session_state": None,
+        }
 
         for key in login_params.keys():
-                login_params[key] = login_soup.find(
-                        "input", {"name": key}).get("value")
+            login_params[key] = login_soup.find("input", {"name": key}).get("value")
 
         # We only care about getting the cookies.
-        _ = self.session.post(
-                url = callback_url,
-                data = login_params)
+        _ = self.session.post(url=callback_url, data=login_params)
 
         # FIXME Make this independent from the login fun, i.e. check if all
         # prerequisites are true.
@@ -79,14 +76,13 @@ class FroniusSession:
             "month": date.month,
             "day": date.day,
             "interval": "day",
-            "view": "production"}
+            "view": "production",
+        }
 
     def get_chart(self, date):
         chart_resp = self.session.get(
-            url = self.chart_url,
-            data = self.chart_data(
-                    id = self.secret["id"],
-                    date = date))
+            url=self.chart_url, data=self.chart_data(id=self.secret["id"], date=date)
+        )
 
         # TODO Add handling of case that chart_resp doesn't contain json.
-        return(chart_resp.json())
+        return chart_resp.json()
