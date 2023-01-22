6878996a75db0cc6d4546b0d06f8d71d8b3a7a26
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2023-01-22 11:30:33 +0000

    Runner: Format with black

--- scrape.py
+++ scrape.py
@@ -10,31 +10,28 @@ from fronius_scraper.fronius_session import FroniusSession
 load_dotenv()
 
 secrets = {
-        "username": os.getenv("username"),
-        "password": os.getenv("password"),
-        "fronius-id": os.getenv("fronius-id")
+    "username": os.getenv("username"),
+    "password": os.getenv("password"),
+    "fronius-id": os.getenv("fronius-id"),
 }
 
 if None in secrets.values():
-        none_secrets = ", ".join(
-                [x[0] for x in secrets.items() if x[1] is None])
+    none_secrets = ", ".join([x[0] for x in secrets.items() if x[1] is None])
 
-        raise Exception(
-                f"Fields {none_secrets} where not filled. Check your .env " \
-                f"file.")
+    raise Exception(
+        f"Fields {none_secrets} where not filled. Check your .env " f"file."
+    )
 
 fsession = FroniusSession(
-        user = secrets["username"],
-        password = secrets["password"],
-        id = secrets["fronius-id"]
+    user=secrets["username"], password=secrets["password"], id=secrets["fronius-id"]
 )
 
-yesterday = dt.date.today() - dt.timedelta(days = 1)
+yesterday = dt.date.today() - dt.timedelta(days=1)
 
-json_out = json.dumps(fsession.get_chart(date = yesterday))
+json_out = json.dumps(fsession.get_chart(date=yesterday))
 
 output_dir = "./"
 output_file = output_dir + yesterday.strftime("%Y%m%d.json")
 
 with open(output_file, "w") as outfile:
-        outfile.write(json_out)
+    outfile.write(json_out)
