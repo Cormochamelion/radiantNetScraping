bef35523ee2f11abb85ac2a2d5eee27b2b523824
cormochamelion <cormocodran1.5@gmail.com>
2024-05-09 12:54:29 +0200

    scrape: Add docstring

--- scrape.py
+++ scrape.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Log into Fronius Solarweb with the credentials in the environment (or .env),
+get the data for the standard dayly plot of production, use, and battery level for the
+previous day, and dump them to a timestamped JSON file.
+"""
 
 import os
 import json
