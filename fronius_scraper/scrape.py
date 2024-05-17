8959aa24a4d37db5bac5de5bffaa4fa9ba627dfd
cormochamelion <cormocodran1.5@gmail.com>
2024-05-17 22:11:52 +0200

    scrape: Factor scraping into own function

--- fronius_scraper/scrape.py
+++ fronius_scraper/scrape.py
@@ -13,6 +13,18 @@ from dotenv import load_dotenv
 from fronius_scraper.fronius_session import FroniusSession
 
 
+def scrape_daily_data(secrets: dict, date: dt.date) -> dict:
+    """
+    Use a login session to obtain the daily data chart for a given date as a serialized
+    JSON dict.
+    """
+    fsession = FroniusSession(
+        user=secrets["username"], password=secrets["password"], id=secrets["fronius-id"]
+    )
+
+    return json.dumps(fsession.get_chart(date=date))
+
+
 def main():
     load_dotenv()
 
@@ -29,13 +41,9 @@ def main():
             f"Fields {none_secrets} were not filled. Check your .env " f"file."
         )
 
-    fsession = FroniusSession(
-        user=secrets["username"], password=secrets["password"], id=secrets["fronius-id"]
-    )
-
     yesterday = dt.date.today() - dt.timedelta(days=1)
 
-    json_out = json.dumps(fsession.get_chart(date=yesterday))
+    json_out = scrape_daily_data(secrets, yesterday)
 
     output_dir = "./"
     output_file = output_dir + yesterday.strftime("%Y%m%d.json")
