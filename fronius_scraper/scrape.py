7b601c6834cd4d27dd03b9c6dbc8d4b0aae51343
cormochamelion <cormocodran1.5@gmail.com>
2024-05-17 22:05:50 +0200

    scrape: Factor everything into main fun

--- fronius_scraper/scrape.py
+++ fronius_scraper/scrape.py
@@ -12,31 +12,37 @@ from dotenv import load_dotenv
 
 from fronius_scraper.fronius_session import FroniusSession
 
-load_dotenv()
 
-secrets = {
-    "username": os.getenv("username"),
-    "password": os.getenv("password"),
-    "fronius-id": os.getenv("fronius-id"),
-}
+def main():
+    load_dotenv()
 
-if None in secrets.values():
-    none_secrets = ", ".join([x[0] for x in secrets.items() if x[1] is None])
+    secrets = {
+        "username": os.getenv("username"),
+        "password": os.getenv("password"),
+        "fronius-id": os.getenv("fronius-id"),
+    }
 
-    raise Exception(
-        f"Fields {none_secrets} where not filled. Check your .env " f"file."
+    if None in secrets.values():
+        none_secrets = ", ".join([x[0] for x in secrets.items() if x[1] is None])
+
+        raise Exception(
+            f"Fields {none_secrets} where not filled. Check your .env " f"file."
+        )
+
+    fsession = FroniusSession(
+        user=secrets["username"], password=secrets["password"], id=secrets["fronius-id"]
     )
 
-fsession = FroniusSession(
-    user=secrets["username"], password=secrets["password"], id=secrets["fronius-id"]
-)
+    yesterday = dt.date.today() - dt.timedelta(days=1)
 
-yesterday = dt.date.today() - dt.timedelta(days=1)
+    json_out = json.dumps(fsession.get_chart(date=yesterday))
 
-json_out = json.dumps(fsession.get_chart(date=yesterday))
+    output_dir = "./"
+    output_file = output_dir + yesterday.strftime("%Y%m%d.json")
 
-output_dir = "./"
-output_file = output_dir + yesterday.strftime("%Y%m%d.json")
+    with open(output_file, "w") as outfile:
+        outfile.write(json_out)
 
-with open(output_file, "w") as outfile:
-    outfile.write(json_out)
+
+if __name__ == "__main__":
+    main()
