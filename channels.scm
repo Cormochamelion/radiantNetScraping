d7940a319448b8052c8e1c2bf2f026be68238f10
cormochamelion <cormocodran1.5@gmail.com>
2024-05-20 10:59:59 +0200

    channels.scm: Remove R repos
      Not currently needed.

--- channels.scm
+++ channels.scm
@@ -8,16 +8,4 @@
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
-        (name 'guix-bioc)
-        (url "https://github.com/guix-science/guix-bioc.git")
-        (branch "master")
-        (commit
-          "9a8f77b57fee63e5e7c52eb82d4a37d368297b61"))
-      (channel
-        (name 'guix-cran)
-        (url "https://github.com/guix-science/guix-cran.git")
-        (branch "master")
-        (commit
-          "63de9b4452dbf24978362c385dec1f366301b4d9")))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
